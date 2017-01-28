e6b40599261bb2151f7a68602aff8175b484c4de
zathershtein <hostel@ukr.net>
2017-01-28 13:22:13 +0200

    Http server added

--- src/js/db.js
+++ src/js/db.js
@@ -1,7 +1,7 @@
 let mysql = require('mysql');
 let connection = mysql.createConnection({
   host: 'localhost',
-  user: '',
+  user: 'finControl',
   password: '',
   database : 'finControl'
 });
@@ -10,9 +10,10 @@ connection.connect();
 
 connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
   if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-  console.log(results);
-  console.log(fields);
+  console.log("Database connected, data is " + results);
+  // console.log('The solution is: ', results[0].solution);
+  // console.log(results);
+  // console.log(fields);
 });
 
 connection.end();
