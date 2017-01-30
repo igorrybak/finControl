29ca6a730109ffdb9124c89b646e1d033d740ac6
zathershtein <hostel@ukr.net>
2017-01-30 07:04:16 +0200

    Added bootstrap

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,17 @@
 console.log("App started...");
 
 let mysqlConnect = require("./db")
-let http = require("http");
+// let http = require("http");
 
-http.createServer((request, response) => {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+// http.createServer((request, response) => {
+//    // Send the HTTP header 
+//    // HTTP Status: 200 : OK
+//    // Content Type: text/plain
+//    response.writeHead(200, {'Content-Type': 'text/plain'});
    
-   // Send the response body as "Hello World"
-   response.end('App in development mode...');
-}).listen(8081);
+//    // Send the response body as "Hello World"
+//    response.end('App in development mode...');
+// }).listen(8081);
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+// // Console will print the message
+// console.log('Server running at http://127.0.0.1:8081/');
